27a3c950d970476f5a9e2c5d823f7683817d73b8
${Mr.DJA} <42304709+iMrDJAi@users.noreply.github.com>
2023-05-28 13:25:05 +0100

    Updated types

--- pdf-img-convert.d.ts
+++ pdf-img-convert.d.ts
@@ -7,6 +7,8 @@ declare function convert (
     height?: number
     /** A list of pages to render instead of all of them */
     page_numbers?: number[]
+    /** Throw error if num of pages exceeds this value */
+    max_pages?: number
     /** Output as base64 */
     base64?: boolean
     /** Viewport scale as ratio */
